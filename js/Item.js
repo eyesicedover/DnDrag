0cc4d81b6a33006e501c12ff634e28c11f0acd4a
Kaila Goff and Stephanie Faber <kaila@>
2018-03-14 11:52:46 -0700

    combine specs to one folder

--- js/Item.js
+++ js/Item.js
@@ -5,13 +5,35 @@ export class Item {
     this.value = 0;
   }
 
+  makeup() {
+    this.name = "makeup";
+    this.type = "fierce";
+    this.value = 2;
+  }
+
+  ductTape() {
+    this.name = "duct tape";
+    this.type = "fierce";
+    this.value = 1;
+  }
+
+  heels() {
+    this.name = "heels";
+    this.type = "fierce";
+    this.value = 2;
+  }
+
   sunglasses() {
     this.name = "sunglasses";
+    this.type = "shade";
+    this.value = 1;
+  }
+
+  alcohol() {
+    this.name = "alcohol";
     this.type = "shade";
     this.value = 2;
   }
 
-  // alcohol() {
-  //
-  // }
+
 }
