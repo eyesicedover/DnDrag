ba90f5ffe067f12402c27fa5a066300075a17379
Kaila Goff and Stephanie Faber <stephanie@>
2018-03-15 09:44:04 -0700

    add test that meaningfully fails for Item Drop in Battle class

--- spec/battle_spec.js
+++ spec/battle_spec.js
@@ -1,6 +1,7 @@
 import { Battle } from './../js/battle.js';
 import { Character } from './../js/character.js';
 import { Enemy } from './../js/enemy.js';
+import { Item } from './../js/item.js';
 
 describe ("Battle", function () {
   it("will take away hp from enemy or player based on action", function() {
@@ -36,6 +37,25 @@ describe ("Battle", function () {
     let winner = newBattle.determineWinner();
 
     expect(winner).toEqual("You lost.... sashay away.");
+  })
+
+  it("will determine an item drop after battle", function() {
+    //Arrange
+    let newPlayer = new Character();
+    newPlayer.createGlamazon();
+    let newEnemy = new Enemy();
+    newEnemy.createImp();
+    newPlayer.action = "slay";
+    let newBattle = new Battle(newPlayer, newEnemy);
+    newBattle.enemy.hp = 0;
+    newBattle.determineWinner();
+    let testItem = new Item();
+
+    //Act
+    let resultItem = newBattle.itemDrop();
+
+    //Assert
+    expect(typeof testItem.value).toEqual(typeof resultItem.name);
   })
 
 })
