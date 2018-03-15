186af47d44299218884c09f618a970bedf3e9f58
Kaila Goff and Stephanie Faber <stephanie@>
2018-03-15 11:53:36 -0700

    add form to index.html and jquery for player name and type

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import { Character } from './../js/character';
 import { Item } from './../js/item';
-import { Battle, roll } from './../js/battle';
+import { Battle} from './../js/battle';
+import { Enemy } from './../js/enemy';
 import './styles.css';
 
 $(document).ready(function() {
   let newCharacter = new Character();
+  $(".createQueen").submit(function(event) {
+    event.preventDefault();
+    let name = $("input#dragName").val();
+    let type = $("input:radio[name=dragClass]:checked").val();
+    newCharacter.name = name;
+    newCharacter.type = type;
+    if (type == "glamazon") {
+      newCharacter.createGlamazon();
+    } else if (type == "cheesecake") {
+      newCharacter.createCheesecake();
+    } else {
+      newCharacter.createHeather();
+    }
+  });
 });
